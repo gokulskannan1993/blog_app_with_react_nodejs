f85ee82be3b94ce67c74886e744d3557f0f04e4f
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-01 09:24:10 +0100

    header completed

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,5 +3,17 @@ import "./header.css";
 import React from "react";
 
 export default function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="titles">
+        <span className="titleSmall">React </span>
+        <span className="titleLarge">Blog</span>
+      </div>
+      <img
+        className="image"
+        src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2019_17/2835276/190425-thor-avengers-endgame-ew-339p.jpg"
+        alt=""
+      ></img>
+    </div>
+  );
 }
