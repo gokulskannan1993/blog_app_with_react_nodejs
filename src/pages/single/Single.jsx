7d9c75000407b22a155c6f93e19d534042e5460c
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-02 13:39:05 +0100

    components moved to a common folder, single create

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./single.css";
+
+export default function Single() {
+  return (
+    <div className="single">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
