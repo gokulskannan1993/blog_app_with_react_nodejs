0993dcf1c13adb3117ef11e43ec58fe4c5b2485c
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-02 10:51:54 +0100

    post styling done

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import "./post.css";
 
 export default function Post() {
-  return <div>Single Post</div>;
+  return (
+    <div className="post">
+      <img
+        className="postImage"
+        src="https://media.istockphoto.com/photos/green-leaf-with-dew-on-dark-nature-background-picture-id1050634172?k=6&m=1050634172&s=612x612&w=0&h=C6CWho9b4RDhCqvaivYOLV2LK6FzygYpAyLPBlF1i2c="
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">Lorem Ipsum is simply dummy</span>
+        <hr></hr>
+        <span className="postDate"> 1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  );
 }
