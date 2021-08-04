62af6ae66deafe49f258e8c5cb03e21bcf38bb54
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-04 14:40:40 +0100

    basic routing done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+
+  
+  const user = false;
+
   return (
     <div className="App">
-      <NavBar></NavBar>
-      {/* <Home></Home> */}
-      {/* <Single></Single> */}
-      {/* <Write></Write> */}
-      {/* <Settings></Settings> */}
-      {/* <Login></Login> */}
-      <Register></Register>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/signup">
+            <Register></Register>
+          </Route>
+          <Route path="/signin">
+            <Login></Login>
+          </Route>
+          <Route path="/settings">
+            <Settings></Settings>
+          </Route>
+          <Route path="/post/:postID">
+            <Single></Single>
+          </Route>
+          <Route path="/write">
+            <Write></Write>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
