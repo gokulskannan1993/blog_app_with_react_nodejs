569d1a63774c3064dd8a8ba509d87f0fc61dad5c
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-05 09:28:35 +0100

    User schema created

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// User Schema
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
