90f1ccc350fdcbd5c2bce1fa7f649ddbc77f4144
Gokul Kannan <gokulskannan1993@gmail.com>
2021-08-05 09:21:48 +0100

    db connection established

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
-
 const app = express();
-
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
 
-
+// Connection to the database in MONGO ATLAS
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // userCreateIndex: true,
+  })
+  .then(console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 app.use("/", (req, res) => {
   console.log("Hello World");
 });
 
-app.listen("8000", () => {
+app.listen("5000", () => {
   console.log("Backend is running");
 });
